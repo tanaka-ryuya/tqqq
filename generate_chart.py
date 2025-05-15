36087ae1321cc062bfb4891069e5f9fbf19a1cf2
tanaka-ryuya <ryuyaicehockey@gmail.com>
2025-05-15 15:07:27 +0900

    mod

--- generate_chart.py
+++ generate_chart.py
@@ -1,18 +1,17 @@
-import tqqq
-import tqqq_graph
-import tqqq_log_graph
-
-
 def generate_tqqq_chart():
+    import tqqq
+    import tqqq_graph
     tqqq.download_tqqq_data()
     tqqq.download_usd_jpy_rate()
     tqqq_graph.generate_tqqq_linear_chart()
 
 def generate_tqqq_chart_log():
+    import tqqq
+    import tqqq_log_graph
     tqqq.download_tqqq_data()
     tqqq.download_usd_jpy_rate()
     tqqq_log_graph.generate_tqqq_chart()
-
+    
 if __name__ == '__main__':
     generate_tqqq_chart()
     generate_tqqq_chart_log()
