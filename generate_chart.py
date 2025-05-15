8b1ebea5e69cbf72943f944318315155eaf9653b
tanaka-ryuya <ryuyaicehockey@gmail.com>
2025-05-15 13:38:52 +0900

    comiit

--- generate_chart.py
+++ generate_chart.py
@@ -12,6 +12,7 @@ def generate_tqqq_chart_log():
     tqqq.download_tqqq_data()
     tqqq.download_usd_jpy_rate()
     tqqq_log_graph.generate_tqqq_chart()
-    
+
 if __name__ == '__main__':
     generate_tqqq_chart()
+    generate_tqqq_chart_log()
